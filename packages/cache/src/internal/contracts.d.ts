d150ad5777011bcbbff3cced513c0bcc9a733c26
LeHieu1996 <157984396+LeHieu1996@users.noreply.github.com>
2024-01-29 01:56:24 +0700

    Initialize robherley/core-1.8.0-release

---
+++ packages/cache/src/internal/contracts.d.ts
@@ -0,0 +1,33 @@
+import {CompressionMethod} from './constants'
+import {ITypedResponse} from '@actions/http-client/interfaces'
+import {HttpClientError} from '@actions/http-client'
+
+export interface ITypedResponseWithError<T> extends ITypedResponse<T> {
+  error?: HttpClientError
+}
+
+export interface ArtifactCacheEntry {
+  cacheKey?: string
+  scope?: string
+  creationTime?: string
+  archiveLocation?: string
+}
+
+export interface CommitCacheRequest {
+  size: number
+}
+
+export interface ReserveCacheRequest {
+  key: string
+  version?: string
+  cacheSize?: number
+}
+
+export interface ReserveCacheResponse {
+  cacheId: number
+}
+
+export interface InternalCacheOptions {
+  compressionMethod?: CompressionMethod
+  cacheSize?: number
+}
